5bf2a01066f728bff35fb5fd0ace2caa171013e8
Gianluca Sestito <gianluca.sestito93@gmail.com>
2023-07-02 12:46:37 +0200

    updated configs for site name and preview

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+// vue.config.js
+module.exports = {
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'The most beautiful in the world'; // Update the site name here
+      args[0].meta = {
+        'og:title': {
+          property: 'og:title',
+          content: 'The most beautiful in the world' // Update the OG title here
+        }
+      };
+      return args;
+    });
+  },
   publicPath: ''
-})
+};
